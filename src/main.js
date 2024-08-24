cf67bbdcfeb58cef245779f324df6c2e7e8d3bb2
Robertarezza <roberta.rezza@gmail.com>
2024-08-24 15:28:27 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import './style/general.scss';
+import './Style/general.scss';
 import App from './App.vue';
 import { router } from './router';
 import 'bootstrap/js/dist/carousel.js'
